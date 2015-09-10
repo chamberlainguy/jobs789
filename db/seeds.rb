353b7cce0fbaa7408469adae650108c26ac015ce
Guy Chamberlain <guychamberlain@Guys-MacBook-Air.local>
2015-09-10 13:11:48 +1000

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,7 @@ e1 = Employer.create :email => "maccas@maccas.com",
 						:name => "MC Donalds",
 						:address => "58 Homestead Rd\nOrchard Hills NSW",
 						:password => "chicken"
-e2 = Employer.create :email => "stooge@ibm.com",
+e2 = Employer.create :email => "ibm@ibm.com",
 						:name => "IBM",
 						:address => "55 York St\nSydney NSW",
 						:password => "chicken"
@@ -57,9 +57,21 @@ e2 = Employer.create :email => "stooge@ibm.com",
 
  Job.destroy_all
  j1 = Job.create :title => "Fry Cook", :location => "East Sydney",
- 				 :salary => 20000,  :industry => "Food",
- 				 :description => "A Fry cook is required to cook burgers and fries from 6am to midnight. Monday to Saturday",
- 				 :closing_date => "14/10/2015"
+         :salary => 20000,  :industry => "Food",
+         :description => "A Fry cook is required to cook burgers and fries from 6am to midnight. Monday to Saturday",
+         :closing_date => "14/10/2015"
+ j2 = Job.create :title => "Trainee Manager", :location => "South Sydney",
+         :salary => 19000,  :industry => "Food",
+         :description => "A Trainee Manager is required to do whatever it is a trainee manager does",
+         :closing_date => "14/11/2015"
+ j3 = Job.create :title => "Burger Griller", :location => "East Sydney",
+         :salary => 23000,  :industry => "Food",
+         :description => "A Burger Griller is required to cook burgers and fries from 6am to midnight. Monday to Saturday",
+         :closing_date => "14/11/2015"
+ j4 = Job.create :title => "Programmer dude", :location => "East Sydney",
+         :salary => 32000,  :industry => "Tech",
+         :description => "A Programmer Dude is required to do whatever it is a programmer dude does",
+         :closing_date => "14/12/2015"
 
 
   # create_table :appls do |t|
@@ -73,14 +85,21 @@ e2 = Employer.create :email => "stooge@ibm.com",
   Appl.destroy_all
   a1 = Appl.create :cover_letter => "I think I'll really fit in well"
   a2 = Appl.create :cover_letter => "I really wanna work here"
+  a3 = Appl.create :cover_letter => "An interesting cover letter"
+  a4 = Appl.create :cover_letter => "An interesting cover letter"
+  a5 = Appl.create :cover_letter => "An interesting cover letter"
 
-  e1.jobs << j1
+
+  e1.jobs << j1 << j2 << j3
+  e2.jobs << j4
 
   j1.appls << a1 << a2
+  j2.appls << a3
+  j3.appls << a4
+  j4.appls << a5
 
   js1.appls << a1
-
-  js2.appls << a2
+  js2.appls << a2 << a3 << a4 << a5
   
 
 
